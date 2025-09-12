b442f067b6ec3fc9cc228365975f0f9d7b9c2de8
Mohamed Raouf Hassan <133545566+Ahmedkhlil12@users.noreply.github.com>
2025-09-12 05:26:26 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["data science", "machine learning", "ai engineering", "automation", "web scraping"],
+    strings: ["Front-End", "Back-End", "Web Development ", "Web Designing", "Freelancing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
